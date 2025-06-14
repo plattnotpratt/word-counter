435e4d7f885821994e0c0a8b6daaaee561808e39
Christopher Platt <plattnotpratt@gmail.com>
2025-06-14 10:49:15 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const textArea = document.getElementById('word-count-area');
+const lineCountElement = document.getElementById('line-count');
+const wordCountElement = document.getElementById('word-count');
+const charCountElement = document.getElementById('char-count');
+
+let charCount = (text) => {
+    return text.split('').length;
+}
+
+let wordCount = (text) => {
+    return text.split(' ').length;
+}
+
+let lineCount = (text) => {
+    return text.split('\n').length;
+}
+
+textArea.addEventListener('input', textChanged)
+
+
+function textChanged(e){
+    wordCountElement.innerHTML = wordCount(e.target.value);
+    charCountElement.innerHTML = charCount(e.target.value);
+    lineCountElement.innerHTML = lineCount(e.target.value);
+}
